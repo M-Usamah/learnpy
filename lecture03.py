27ed6b5af9e5ef40ae5400bc88eb1643eb7121a7
M-Usamah <mohammad.usamah127@hootmail.com>
2024-01-21 14:50:31 +0500

    lecture 03 create a loop to stop the window to close imediately

---
+++ lecture03.py
@@ -0,0 +1,13 @@
+import pygame
+x = pygame.init()
+gameWindow = pygame.display.set_mode((1200,500))
+pygame.display.set_caption("My First Game")
+
+exit_game = False
+game_over = False
+
+while not exit_game:
+    pass
+
+pygame.quit()
+quit()
